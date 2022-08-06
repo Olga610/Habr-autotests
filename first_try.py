c45d76d1251e1fc6e26f9a3490fd15eb689ea0b9
Olga <olgabogova610@gmail.com>
2022-08-06 19:04:05 +0300

    add construction if __name__ == '__main__'

--- first_try.py
+++ first_try.py
@@ -3,41 +3,53 @@ import time
 from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common.by import By
 
-# settings
-driver = webdriver.WebDriver(executable_path="chromedriver.exe")
-driver.get("https://habr.com")
-time.sleep(2)
 
-# поиск
-search_button_locator = By.CLASS_NAME, 'tm-header-user-menu__search'
-search_button = driver.find_element(*search_button_locator)
-search_button.click()
-time.sleep(2)
+def setup():
+    driver = webdriver.WebDriver(executable_path="chromedriver.exe")
+    driver.get("https://habr.com")
+    time.sleep(2)
 
-# ввод текста
-search_input_locator = By.CSS_SELECTOR, 'input.tm-input-text-decorated__input'
-search_input = driver.find_element(*search_input_locator)
-text_to_search = 'QA'
-search_input.send_keys(text_to_search)
-time.sleep(2)
+    return driver
 
-# нажатие на иконку поиска
-search_icon_locator = By.CLASS_NAME, 'tm-search__icon'
-search_icon = driver.find_element(*search_icon_locator)
-search_icon.click()
-time.sleep(2)
+def tear_down(driver):
+    driver.quit()
 
-# кол-во записей
-article_locator = By.TAG_NAME, 'article'
-articles = driver.find_elements(*article_locator)
-print(f'number of articles is {len(articles)}')
-time.sleep(1)
+def test_basic_search(driver):
+    # поиск
+    search_button_locator = By.CLASS_NAME, 'tm-header-user-menu__search'
+    search_button = driver.find_element(*search_button_locator)
+    search_button.click()
+    time.sleep(2)
 
-# кол-во страниц
-last_page_locator = By.XPATH, '(//*[@class="tm-pagination__page"])[last()]'
-last_page_number = driver.find_element(*last_page_locator)
-element_text = last_page_number.text
-print(f'number of pages is {element_text}')
-time.sleep(1)
+    # ввод текста
+    search_input_locator = By.CSS_SELECTOR, 'input.tm-input-text-decorated__input'
+    search_input = driver.find_element(*search_input_locator)
+    text_to_search = 'QA'
+    search_input.send_keys(text_to_search)
+    time.sleep(2)
 
-driver.quit()
+    # нажатие на иконку поиска
+    search_icon_locator = By.CLASS_NAME, 'tm-search__icon'
+    search_icon = driver.find_element(*search_icon_locator)
+    search_icon.click()
+    time.sleep(2)
+
+    # кол-во записей
+    article_locator = By.TAG_NAME, 'article'
+    articles = driver.find_elements(*article_locator)
+    print(f'number of articles is {len(articles)}')
+    time.sleep(1)
+
+    # кол-во страниц
+    last_page_locator = By.XPATH, '(//*[@class="tm-pagination__page"])[last()]'
+    last_page_number = driver.find_element(*last_page_locator)
+    element_text = last_page_number.text
+    print(f'number of pages is {element_text}')
+    time.sleep(1)
+
+if __name__ == '__main__':
+    driver = setup()
+
+    test_basic_search(driver)
+
+    tear_down(driver)
