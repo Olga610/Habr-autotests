3e5277b510c2a71e0e0631c4af8944c1edfa067f
Olga <olgabogova610@gmail.com>
2022-08-07 09:07:44 +0300

    add test_3.py

---
+++ test_3.py
@@ -0,0 +1,16 @@
+from first_try import *
+
+def test_main_page(driver):
+    count_articles_number(driver)
+    count_pages_number(driver)
+
+
+if __name__ == '__main__':
+    driver_object = setup()
+
+    try:
+        test_main_page(driver_object)
+    except NoSuchElementException as error:
+        print(f'Test failed, {error}')
+
+    tear_down(driver_object)
