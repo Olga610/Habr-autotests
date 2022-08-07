5ea30af8ed393cfff539f1719c3ea208c04de0ec
Olga <olgabogova610@gmail.com>
2022-08-07 09:49:58 +0300

    add file page.py with all test settings

--- test_2.py
+++ test_2.py
@@ -1,23 +1,12 @@
-from test_1 import *
-
+from page import *
 
 def test_empty_search(driver):
     click_search_form(driver)
-
     type_text(driver, 'asdfgfdsa')
-
     click_search_button(driver)
-
     count_articles_number(driver)
-
     check_empty_page_text(driver)
 
-
-def check_empty_page_text(driver):
-    empty_res_locator = By.XPATH, '(//*[@data-test-id="empty-placeholder-text"])'
-    empty_results = driver.find_element(*empty_res_locator)
-    print(f'Text on page: {empty_results.text}')
-    time.sleep(1)
 
 if __name__ == '__main__':
     driver_object = setup()
@@ -25,6 +14,6 @@ if __name__ == '__main__':
     try:
         test_empty_search(driver_object)
     except NoSuchElementException as error:
-        print('Test failed, {error}')
+        print(f'Test failed, {error}')
 
     tear_down(driver_object)
