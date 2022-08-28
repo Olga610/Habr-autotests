6968619dd91bdb064090c200c0a6c03bedb0edec
Olga <olgabogova610@gmail.com>
2022-08-28 19:12:09 +0300

    add pytest fixture to tests for opening

--- TESTS/search_tests/test_empty_search.py
+++ TESTS/search_tests/test_empty_search.py
@@ -1,15 +1,11 @@
 from page_objects.page import *
 
-def test_empty_search(driver):
-    page = MainPage(driver)
-    page.open()
-    page = page.click_search()
+def test_empty_search(page):
     page.search('asdfgfdsa')
+
     assert page.count_articles_number() == 0
 
-def test_empty_search_text(driver):
-    page = MainPage(driver)
-    page.open()
-    page = page.click_search()
+def test_empty_search_text(page):
     page.search('asdfgfdsa')
+
     assert len(page.get_empty_page_text()) > 0
