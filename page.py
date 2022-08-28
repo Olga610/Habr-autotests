073898662ede3cbcd645875da933bc631401564d
Olga <olgabogova610@gmail.com>
2022-08-28 08:40:16 +0300

    add PageObject in page.py

--- page.py
+++ page.py
@@ -12,42 +12,55 @@ class MainPage:
     def open(self):
         self.__webdriver.get(self.url)
 
-def count_pages_number(driver):
-    last_page_number = driver.find_element(*last_page_locator)
-    element_text = last_page_number.text
-    print(f'number of pages is {element_text}')
-    time.sleep(1)
+    @property
+    def search_icon(self):
+        return self.__webdriver.find_element(*search_icon_locator)
 
-def go_to_last_page(driver):
-    last_page = driver.find_element(*last_page_locator)
-    last_page.click()
-    time.sleep(6)
+    def click_search(self):
+        self.search_icon.click()
+        time.sleep(2)
 
-def count_articles_number(driver):
-    articles = driver.find_elements(*article_locator)
-    print(f'number of articles is {len(articles)}')
-    time.sleep(1)
+        return SearchResultsPage(self.__webdriver)
 
+class SearchResultsPage:
+    url = "https://habr.com/ru/search"
 
-def click_search_button(driver):
-    search_icon = driver.find_element(*search_icon_locator)
-    search_icon.click()
-    time.sleep(2)
+    def __init__(self, webdriver):
+        self.__webdriver = webdriver
 
+    @property
+    def search_input(self):
+        return self.__webdriver.find_element(*search_input_locator)
 
-def type_text(driver, text):
-    search_input = driver.find_element(*search_input_locator)
-    text_to_search = text
-    search_input.send_keys(text_to_search)
-    time.sleep(2)
+    @property
+    def search_button(self):
+        return self.__webdriver.find_element(*search_button_locator)
 
 
-def click_search_form(driver):
-    search_button = driver.find_element(*search_button_locator)
-    search_button.click()
-    time.sleep(2)
+    def search(self, search_text):
+        self.search_input.send_keys(text_to_search)
+        self.search_button.click()
 
-def check_empty_page_text(driver):
-    empty_results = driver.find_element(*empty_res_locator)
-    print(f'Text on page: {empty_results.text}')
-    time.sleep(1)
+        time.sleep(2)
+
+
+    def count_pages_number(self):
+        last_page_number = self.__webdriver.find_element(*last_page_locator)
+        element_text = last_page_number.text
+        print(f'number of pages is {element_text}')
+        time.sleep(1)
+
+    def go_to_last_page(self):
+        last_page = self.__webdriver.find_element(*last_page_locator)
+        last_page.click()
+        time.sleep(6)
+
+    def count_articles_number(self):
+        articles = self.__webdriver.find_elements(*article_locator)
+        print(f'number of articles is {len(articles)}')
+        time.sleep(1)
+
+    def check_empty_page_text(self):
+        empty_results = self.__webdriver.find_element(*empty_res_locator)
+        print(f'Text on page: {empty_results.text}')
+        time.sleep(1)
